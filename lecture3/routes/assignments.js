8c8b0959c0710f8d541c650e17a7fe0b9dc8d22a
numso <dosmun@gmail.com>
2013-10-04 14:17:50 -0600

    added hw for l3

--- lecture3/routes/assignments.js
+++ lecture3/routes/assignments.js
@@ -10,45 +10,20 @@ module.exports = function (app) {
 function getHW(app) {
   var hw = app.data.hw;
   return function (req, res) {
-    // timeout to simulate async
-    setTimeout(function () {
-      res.send({
-        success: true,
-        hw: hw
-      });
-    }, 200);
+    // TODO:: Return a list of homework
   };
 }
 
 function postHW(app) {
   var hw = app.data.hw;
   return function (req, res) {
-    var id = Math.floor(Math.random() * 999999);
-    hw.push({
-      name: req.body.name,
-      dueDate: req.body.dueDate,
-      id: id
-    });
-
-    res.send({
-      success: true,
-      id: id
-    });
+    // TODO:: Add homework to the hw array
   };
 }
 
 function deleteHW(app) {
   var hw = app.data.hw;
   return function (req, res) {
-    for (var i = 0; i < hw.length; ++i) {
-      if (hw[i].id == req.body.id) {
-        hw.splice(i, 1);
-        return res.send({ success: true });
-      }
-    }
-    res.send({
-      success: false,
-      err: 'Couldn\'t find that hw.'
-    });
+    // TODO:: Delete homework from the hw array
   };
 }
--- lecture3/routes/grades.js
+++ lecture3/routes/grades.js
@@ -2,32 +2,9 @@ module.exports = function (app) {
   app.data = app.data || {};
   app.data.grades = {};
 
-  app.get('/grades', app.middleware.security, getGrades(app));
-  app.get('/grades/:name', app.middleware.security, getGrades(app));
-  app.post('/grade', app.middleware.security, postGrade(app));
+  // TODO:: Set up the following routes:
+
+  // app.get('/grades', fn);
+  // app.get('/grades/:name', fn2);
+  // app.post('/grade', fn3);
 };
-
-function postGrade(app) {
-  var grades = app.data.grades;
-  return function (req, res) {
-    var name = req.session.user.user;
-    grades[name] = grades[name] || {};
-    grades[name][req.body.id] = req.body.complete == 'true';
-
-    res.send({
-      success: true
-    });
-  };
-}
-
-function getGrades(app) {
-  var grades = app.data.grades;
-  return function (req, res) {
-    var name = req.params.name || req.session.user.user;
-
-    res.send({
-      success: true,
-      grades: grades[name] || {}
-    });
-  };
-}
